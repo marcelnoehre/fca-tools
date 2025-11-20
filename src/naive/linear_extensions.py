8dc9a25c16d0f41cc23b6046b0f6ab102359157d
marcelnoehre <marcelnoehre@web.de>
2025-11-20 15:06:58 +0100

    compute all realizers for a list of linear extensions

--- src/naive/linear_extensions.py
+++ src/naive/linear_extensions.py
@@ -1,5 +1,8 @@
-from typing import Set, Tuple, List
 import networkx as nx
+
+from typing import Set, Tuple, List
+from itertools import combinations
+
 from src.utils.constants import *
 from src.naive.lattice import construct_closure
 
@@ -27,3 +30,41 @@ def all_linear_extensions(
                 yield from extend(partial + [n])
 
     return list(extend([]))
+
+def all_minimal_realizers(linear_extensions: List[List], elements: List, poset_closure: Set[Tuple]):
+    '''
+    Find all minimal realizers from a list of linear extensions.
+
+    Args:
+        linear_extensions (List[List]): A list of linear extensions, each represented as a list of elements.
+        elements (List): A list of elements in the poset.
+        poset_closure (Set[Tuple]): A set of tuples representing the order relations in the poset.
+
+    Returns:
+        Tuple[int, List[List]]: A tuple containing the size of the minimal realizer and a list of all minimal realizers.     
+    '''
+
+    def _intersection_of_orders(orders, elements):
+        elements_list = list(elements)
+        pair_all = set()
+        
+        for i in elements_list:
+            for j in elements_list:
+                if i == j:
+                    continue
+                # check if i < j in all orders
+                if all(order.index(i) < order.index(j) for order in orders):
+                    pair_all.add((i, j))
+
+        _, poset_closure = construct_closure(elements_list, pair_all)
+        
+        return poset_closure
+    
+    for k in range(1, len(linear_extensions) + 1):
+        valid_sets = [
+            k_linear_extensions
+            for k_linear_extensions in combinations(linear_extensions, k)
+            if _intersection_of_orders(k_linear_extensions, elements) == poset_closure
+        ]
+        if valid_sets:
+            return k, valid_sets
