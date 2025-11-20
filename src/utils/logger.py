ed6d319c5fa716ac9f8118a04b7a34f5ea17db76
marcelnoehre <marcelnoehre@web.de>
2025-11-20 15:06:20 +0100

    update docstring for logger

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -6,5 +6,9 @@ def log(
     ) -> None:
     '''
     Print a highlighted message to the console.
+
+    Args:
+        msg (str): The message to print.
+        color (str, optional): ANSI color code for highlighting. Defaults to None.
     '''
     print(f'{color}{msg}{RESET}')
