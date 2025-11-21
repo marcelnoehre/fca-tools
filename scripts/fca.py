42d9d2698e127e2da009e5494f6b77eeb3c844a9
marcelnoehre <marcelnoehre@web.de>
2025-11-21 10:39:20 +0100

    compute linear extensions

--- scripts/fca.py
+++ scripts/fca.py
@@ -8,9 +8,11 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 from src.utils.constants import N5_PATH
 from src.fca.formal_context import formal_context
-from src.fca.concept_lattice import concept_lattice
+from src.fca.concept_lattice import concept_lattice, linear_extensions
 from src.fca.plot import plot_concept_lattice_vsl
 
 fc = formal_context(os.path.join(BASE_DIR, N5_PATH))
 cl = concept_lattice(fc)
 plot_concept_lattice_vsl(cl, 'n5_lattice.png')
+for le in linear_extensions(cl):
+    print(le)
--- src/fca/concept_lattice.py
+++ src/fca/concept_lattice.py
@@ -1,5 +1,10 @@
+import networkx as nx
+from typing import List
 from fcapy.context import FormalContext
 from fcapy.lattice import ConceptLattice
 
 def concept_lattice(formal_context: FormalContext) -> ConceptLattice:
     return ConceptLattice.from_context(formal_context)
+
+def linear_extensions(concept_lattice: ConceptLattice) -> List[List[int]]:
+    return list(nx.all_topological_sorts(concept_lattice.to_networkx()))
