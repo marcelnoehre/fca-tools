2c0491103058e5739c2220f41d06b1aba268ae06
marcelnoehre <marcelnoehre@web.de>
2025-11-21 11:03:30 +0100

    compute minimal realizers

--- scripts/fca.py
+++ scripts/fca.py
@@ -7,12 +7,15 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname('src'), '..')))
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 from src.utils.constants import N5_PATH
-from src.fca.formal_context import formal_context
-from src.fca.concept_lattice import concept_lattice, linear_extensions
+from src.naive.linear_extensions import all_minimal_realizers
+from src.fca.formal_context import *
+from src.fca.concept_lattice import *
 from src.fca.plot import plot_concept_lattice_vsl
 
 fc = formal_context(os.path.join(BASE_DIR, N5_PATH))
 cl = concept_lattice(fc)
 plot_concept_lattice_vsl(cl, 'n5_lattice.png')
-for le in linear_extensions(cl):
-    print(le)
+k, realizers = all_minimal_realizers(linear_extensions(cl), cl.to_networkx().nodes, transitive_closure(cl))
+print(f'Minimal realizer size: {k}')
+for realizer in realizers:
+    print(realizer)
--- src/fca/concept_lattice.py
+++ src/fca/concept_lattice.py
@@ -1,5 +1,5 @@
 import networkx as nx
-from typing import List
+from typing import List, Set, Tuple
 from fcapy.context import FormalContext
 from fcapy.lattice import ConceptLattice
 
@@ -8,3 +8,9 @@ def concept_lattice(formal_context: FormalContext) -> ConceptLattice:
 
 def linear_extensions(concept_lattice: ConceptLattice) -> List[List[int]]:
     return list(nx.all_topological_sorts(concept_lattice.to_networkx()))
+
+def cover_relations(concept_lattice: ConceptLattice) -> Set[Tuple[int, int]]:
+    return set(nx.transitive_reduction(concept_lattice.to_networkx()).edges)
+
+def transitive_closure(concept_lattice: ConceptLattice) -> Set[Tuple[int, int]]:
+    return set(nx.transitive_closure(concept_lattice.to_networkx()).edges)
